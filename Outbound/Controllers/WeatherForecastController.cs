de8159893dab343e906b236b2a7add27b4240e36
Suyash Shukla <suyash.s@keka.com>
2021-09-14 09:25:53 +0530

    Add project files.

---
+++ Outbound/Controllers/WeatherForecastController.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Outbound.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        private readonly ILogger<WeatherForecastController> _logger;
+
+        public WeatherForecastController(ILogger<WeatherForecastController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IEnumerable<WeatherForecast> Get()
+        {
+            var rng = new Random();
+            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Date = DateTime.Now.AddDays(index),
+                TemperatureC = rng.Next(-20, 55),
+                Summary = Summaries[rng.Next(Summaries.Length)]
+            })
+            .ToArray();
+        }
+
+        /// <summary>
+        /// Gets the current out bound ip.
+        /// </summary>
+        /// <returns>
+        /// Public IP
+        /// </returns>
+        [HttpGet("outboundip")]
+        public string GetCurrentOutBoundIp()
+        {
+            return new System.Net.WebClient().DownloadString("https://ipv4.icanhazip.com");
+        }
+    }
+}
